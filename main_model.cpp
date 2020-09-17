8a13d51966be00b7d83b2c23d37d43775674c192
Nikhil Krishnan <npk13@case.edu>
2020-09-17 10:19:59 -0400

    initialize main script

---
+++ main_model.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <math.h>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <array>
+#include <vector>
+#include <random>
+#include <ctime> 
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
